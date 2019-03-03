d9b74f7fc546ad61532ed80100ca9e92ec2578c1
quentin <19900809qal>
2019-03-03 11:36:22 +0800

    reformat file

--- tick_data.py
+++ tick_data.py
@@ -8,7 +8,7 @@ import time
 import datetime
 import tushare as ts
 import numpy as np
-from sqlalchemy import create_engine 
+from sqlalchemy import create_engine
 
 symbols = ['600000', '600008', '600848', '600023', '300199', '300001', '002252']
 mysql_engine = create_engine('mysql://root:abc123@localhost:3306/demos?charset=utf8&use_unicode=1')
@@ -16,6 +16,8 @@ mysql_engine = create_engine('mysql://root:abc123@localhost:3306/demos?charset=u
 """
 获取实时行情
 """
+
+
 def get_realtime_price():
     for _ in range(3):
         try:
@@ -29,16 +31,17 @@ def get_realtime_price():
         else:
             return df
 
-    
+
 def run():
-    today  = datetime.datetime.today().date()
+    today = datetime.datetime.today().date()
     today = str(today).replace('-', '')
     last = None
     while True:
         hour = datetime.datetime.today().hour
         minute = datetime.datetime.today().minute
         second = datetime.datetime.today().second
-        if (hour > 15) or (hour == 9 and minute < 30) or (hour < 9) or (hour > 11 and hour < 13) or (hour == 11 and minute > 30):
+        if (hour > 15) or (hour == 9 and minute < 30) or (hour < 9) or (hour > 11 and hour < 13) or (
+                hour == 11 and minute > 30):
             print('未开盘或已收市...')
             break
         else:
@@ -48,7 +51,7 @@ def run():
             else:
                 df = this - last
                 last = this
-                df = df[(df.volume > 0) & (df.amount > 0) ]
+                df = df[(df.volume > 0) & (df.amount > 0)]
                 df.columns = ['change', 'volume', 'amount']
                 this = this[['price']]
                 df = df.merge(this, left_index=True, right_index=True, how='left')
@@ -56,13 +59,13 @@ def run():
                 df['volume'] = df['volume'].astype(int)
                 if df.shape[0] > 0:
                     df['date'] = today
-                    df['time'] = '%s:%s:%s'%(str(hour).zfill(2), str(minute).zfill(2), str(second).zfill(2))
+                    df['time'] = '%s:%s:%s' % (str(hour).zfill(2), str(minute).zfill(2), str(second).zfill(2))
                     df = df.reset_index()
-                    df['type'] = df['change'].map(lambda x: np.where(x>=0, '买入', '卖出'))
-#                     df.to_sql('STOCK_TICK', mysql_engine, index=False, if_exists='append')
+                    df['type'] = df['change'].map(lambda x: np.where(x >= 0, '买入', '卖出'))
+                    #                     df.to_sql('STOCK_TICK', mysql_engine, index=False, if_exists='append')
                     print(df)
         time.sleep(3)
-        
+
 
 def tick_test():
     df = ts.get_tick_data('300199', date='2019-01-22', src='tt')
@@ -71,11 +74,7 @@ def tick_test():
     df.to_sql('STOCK_TICK', mysql_engine, index=False, if_exists='append')
     print(df)
 
+
 if __name__ == '__main__':
     run()
-#     tick_test()    
-    
-    
-    
-    
-    
+#     tick_test()
