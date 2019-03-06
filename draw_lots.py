6a44eed72ebd46630545f3134db00393190c5993
quentin <19900809qal>
2019-03-06 19:38:46 +0800

    增加抽签代码

---
+++ draw_lots.py
@@ -0,0 +1,4 @@
+start = 100008750311
+
+for i in range(0, 1000):
+    print(start + i)
---
+++ monitor.py
@@ -0,0 +1,29 @@
+import tushare as ts
+import redis
+
+# df_hs300 = ts.get_hs300s()
+
+df_sz50 = ts.get_sz50s()
+
+# df_zz500 = ts.get_zz500s()
+
+pool = redis.ConnectionPool(host='127.0.0.1', port='6379')
+
+r = redis.Redis(connection_pool=pool)
+
+# for index, row in df_hs300.iterrows():
+#     print(row['code'], row['name'])
+#     r.set(row['code'], row['name'])
+
+# for index, row in df_zz500.iterrows():
+#     print(row['code'], row['name'])
+#     r.set(row['code'], row['name'])
+    # if r.get(row['code']) is not None:
+    #     print(index, row['code'], row['name'])
+
+# if r.get('600001') is not None:
+#     print(r.get('600000').decode('utf8'))
+# else:
+#     print("没找到")
+
+print(df_sz50)
