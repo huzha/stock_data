8692419b92b3453e32ec0e83db4a914f4a9baede
Allen.Qu <allen.qu@pkfare.com>
2020-05-21 19:21:19 +0800

    上证50

--- sz_50.py
+++ sz_50.py
@@ -12,27 +12,6 @@ df_sz50 = ts.get_sz50s()
 
 engine = create_engine('sqlite:///stock.db', echo=True)
 
-df_sz50.to_sql('sz_50', engine, if_exists='append')
+df_sz50.to_sql('sz_50', engine, if_exists='replace')
 
-# df_zz500 = ts.get_zz500s()
-
-# pool = redis.ConnectionPool(host='127.0.0.1', port='6379')
-
-# r = redis.Redis(connection_pool=pool)
-
-# for index, row in df_hs300.iterrows():
-#     print(row['code'], row['name'])
-#     r.set(row['code'], row['name'])
-
-# for index, row in df_zz500.iterrows():
-#     print(row['code'], row['name'])
-#     r.set(row['code'], row['name'])
-# if r.get(row['code']) is not None:
-#     print(index, row['code'], row['name'])
-
-# if r.get('600001') is not None:
-#     print(r.get('600000').decode('utf8'))
-# else:
-#     print("没找到")
-
-# print(df_sz50)
+print(df_sz50)
